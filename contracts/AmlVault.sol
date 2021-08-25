8dc3cb9914899ed45b5c48b04e547e8d6859e563
facu <facundogregori@hotmail.com>
2021-08-25 09:40:04 -0300

    add MoC to PoC of amlt vault

--- contracts/AmlVault.sol
+++ contracts/AmlVault.sol
@@ -1,53 +1,175 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.7.4; // See "Solidity version" of README.md
+pragma solidity ^0.7.4;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./IETHAtomicOracle.sol";
+import "./IMoC.sol";
 import "./AddressLibrary.sol";
 import "./StatusLibrary.sol";
 
-/**
- * @title An Example Client Smart Contract: a simple vault
- * @author Ville Sundell <development@solarius.fi>
- * @dev This is an example implementation of a client smart contract.
- *
- * This is just to demonstrate Oracle <-> Client communication.
- * The business logic presented here is not viable.
- *
- * THIS IS SIMPLIFIED INTENTIONALLY, AND NOT USABLE IN PRODUCTION AS-IS!
- */
 contract AmlVault {
-    using Address for address payable;
-    using SafeMath for uint256; // Applicable only for uint256
-    // These are Atomic Oracle specific, copy these to your implementation:
     using AddressLibrary for address;
     using StatusLibrary for bytes32;
+    /**
+     * @dev Interface of the oracle
+     */
+    IETHAtomicOracle public oracle;
+    /**
+     * @dev Interface of MoC
+     */
+    IMoC public moc;
+    /**
+     * @dev default value mask of oracle
+     */
+    bytes32 defaultOracleMask;
+    /**
+     * @dev default clean value mask of oracle
+     */
+    bytes32 clearFlagOracleMask;
+    /**
+     * @dev Address of the guardian
+     */
+    address public guardian;
+    /**
+     * @dev Address of the pending guardian
+     */
+    address public pendingGuardian;
+    /**
+     * @dev Emitted when pendingGuardian is changed
+     */
+    event NewPendingGuardian(address oldPendingGuardian, address newPendingGuardian);
+    /**
+     * @dev Emitted when moc is changed
+     */
+    event NewMoC(address oldMoC, address newMoC);
+    /**
+     * @dev Emitted when oracle is changed
+     */
+    event NewOracle(address oldOracle, address newOracle);
+    /**
+     * @dev Emitted when pendingGuardian is accepted, which means gaurdian is updated
+     */
+    event NewGuardian(address oldGuardian, address newGuardian);
+    /**
+     * @dev Emitted user's address is denied
+     */
+    event AddressDenied(address user, uint8 flag);
 
-    IETHAtomicOracle private _oracle;
-    mapping(address => uint256) private _balances;
-
-    constructor(IETHAtomicOracle oracle_) {
-        _oracle = oracle_;
+    constructor(
+        address oracle_,
+        address moc_,
+        bytes32 defaultOracleMask_,
+        bytes32 clearFlagOracleMask_
+    ) {
+        oracle = IETHAtomicOracle(oracle_);
+        moc = IMoC(moc_);
+        guardian = msg.sender;
+        defaultOracleMask = defaultOracleMask_;
+        clearFlagOracleMask = clearFlagOracleMask_;
     }
 
-    function receive() external payable {
-        _balances[msg.sender] = _balances[msg.sender].add(msg.value);
-        _verifyUser(msg.sender);
+    /**
+     * @dev Begins transfer of gaurdian rights. The newPendingGaurdian must call `_acceptAdmin` to finalize the transfer.
+     * @param newPendingGuardian New pending gaurdian.
+     */
+    function _setPendingAdmin(address newPendingGuardian) public {
+        // Check caller = gaurdian
+        require(msg.sender == guardian, "AML: only guardian may set the address");
+        require(newPendingGuardian != address(0), "AML: address admin can not be 0");
+        // Save current value, if any, for inclusion in log
+        address oldPendingGuardian = guardian;
+        // Store pendingGaurdian with value newPendingGaurdian
+        pendingGuardian = newPendingGuardian;
+        emit NewPendingGuardian(oldPendingGuardian, newPendingGuardian);
     }
 
-    function withdraw() external {
-        uint256 balance = _balances[msg.sender];
-        _balances[msg.sender] = 0;
+    /**
+     * @dev  Accepts transfer of gaurdian rights. msg.sender must be pendingGaurdian
+     */
+    function _acceptAdmin() public {
+        // Check caller is pendingGaurdian and pendingGaurdian ≠ address(0)
+        require(msg.sender == pendingGuardian, "AML: only guardian may set the address");
+        require(msg.sender != address(0), "AML: sender can not be 0");
+
+        // Save current values for inclusion in log
+        address oldGuardian = guardian;
+        address oldPendingGaurdian = pendingGuardian;
+
+        // Store gaurdian with value pendingGaurdian
+        guardian = pendingGuardian;
+
+        // Clear the pending value
+        pendingGuardian = address(0);
+
+        emit NewGuardian(oldGuardian, guardian);
+        emit NewPendingGuardian(oldPendingGaurdian, pendingGuardian);
+    }
+
+    /**
+     * @dev Set a new address of IMoC
+     * @param newMoC New address of MoC.
+     */
+    function setMoC(address newMoC) public {
+        // Check caller = gaurdian
+        require(msg.sender == guardian, "AML: only guardian may set the MoC");
+        require(newMoC != address(0), "AML: address MoC can not be 0");
+        // Save current value, if any, for inclusion in log
+        address oldMoC = address(moc);
+        moc = IMoC(newMoC);
+        emit NewMoC(oldMoC, address(moc));
+    }
+
+    /**
+     * @dev Set a new address of oracle
+     * @param newOracle New address of MoC.
+     */
+    function setOracle(address newOracle) public {
+        // Check caller = gaurdian
+        require(msg.sender == guardian, "AML: only guardian may set the oracle");
+        require(newOracle != address(0), "AML: address oreacle can not be 0");
+        // Save current value, if any, for inclusion in log
+        address oldOracle = address(oracle);
+        oracle = IETHAtomicOracle(newOracle);
+        emit NewOracle(oldOracle, address(oracle));
+    }
+
+    /**
+     * @dev Get a fee address to the oracle
+     * @param user address of user.
+     */
+    function getFeeStatus(address user) external returns (uint256) {
+        return _getOracleFee(user);
+    }
+
+    /**
+     * remove it, only for POC
+     */
+    function validateUser(address user) external payable returns (bool) {
+        _verifyUser(user);
+        return true;
+    }
+
+    function mintDoc(uint256 btcToMint) external payable {
         _verifyUser(msg.sender);
-        msg.sender.sendValue(balance);
+        return moc.mintDoc(btcToMint);
+    }
+
+    function redeemFreeDoc(uint256 docAmount) external payable {
+        _verifyUser(msg.sender);
+        return moc.redeemFreeDoc(docAmount);
+    }
+
+    function _getOracleFee(address user) internal returns (uint256) {
+        string memory target = user.toString();
+        return oracle.getFee(address(this));
     }
 
     function _verifyUser(address user) internal {
         string memory target = user.toString();
-        uint256 fee = _oracle.getFee(address(this));
-        bytes32 result = _oracle.getStatusForETH{value: fee}(target);
-        require(!result.isFlag(StatusLibrary.SANCTIONED_FLAG), "ExampleVault: User is flagged as sanctioned");
+        uint256 fee = _getOracleFee(user);
+        bytes32 result = oracle.getStatusForETH{value: fee}(target);
+        require(((result == defaultOracleMask) || (result == clearFlagOracleMask)), "AmlVault: User is sanctioned");
     }
 }
